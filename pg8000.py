cd99acfdeae412c47955dd34351f2dcaacd42273
mfenniak <devnull@localhost>
2007-03-07 20:47:04 -0700

    [svn] add client encoding support

--- pg8000.py
+++ pg8000.py
@@ -265,7 +265,7 @@ class Protocol(object):
             return val
 
     class Bind(object):
-        def __init__(self, portal, ps, in_fc, params, out_fc):
+        def __init__(self, portal, ps, in_fc, params, out_fc, client_encoding):
             self.portal = portal
             self.ps = ps
             self.in_fc = in_fc
@@ -277,7 +277,7 @@ class Protocol(object):
                     fc = self.in_fc[0]
                 else:
                     fc = self.in_fc[i]
-                self.params.append(Types.pg_value(params[i], fc))
+                self.params.append(Types.pg_value(params[i], fc, client_encoding))
             self.out_fc = out_fc
 
         def serialize(self):
@@ -550,24 +550,26 @@ class Protocol(object):
 
     class Connection(object):
         def __init__(self, host=None, port=5432):
-            self.state = "unconnected"
-            self.host = host
-            self.port = port
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._state = "unconnected"
+            self._client_encoding = "ascii"
+            self._host = host
+            self._port = port
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._backend_key_data = None
 
         def verifyState(self, state):
-            if self.state != state:
-                raise InternalError("connection state must be %s, is %s" % (state, self.state))
+            if self._state != state:
+                raise InternalError("connection state must be %s, is %s" % (state, self._state))
 
         def _send(self, msg):
-            self.sock.send(msg.serialize())
+            self._sock.send(msg.serialize())
 
         def _read_message(self):
-            bytes = self.sock.recv(5)
+            bytes = self._sock.recv(5)
             assert len(bytes) == 5
             message_code = bytes[0]
             data_len = struct.unpack("!i", bytes[1:])[0] - 4
-            bytes = self.sock.recv(data_len)
+            bytes = self._sock.recv(data_len)
             msg = Protocol.message_types[message_code].createFromData(bytes)
             if isinstance(msg, Protocol.NoticeResponse):
                 # ignore NoticeResponse
@@ -577,8 +579,8 @@ class Protocol(object):
 
         def connect(self):
             self.verifyState("unconnected")
-            self.sock.connect((self.host, self.port))
-            self.state = "noauth"
+            self._sock.connect((self._host, self._port))
+            self._state = "noauth"
 
         def authenticate(self, user, **kwargs):
             self.verifyState("noauth")
@@ -586,26 +588,31 @@ class Protocol(object):
             msg = self._read_message()
             if isinstance(msg, Protocol.AuthenticationRequest):
                 if msg.ok(self, user, **kwargs):
-                    self.state = "auth"
-                    self._waitForReady()
+                    self._state = "auth"
+                    while 1:
+                        msg = self._read_message()
+                        if isinstance(msg, Protocol.ReadyForQuery):
+                            # done reading messages
+                            self._state = "ready"
+                            break
+                        elif isinstance(msg, Protocol.ParameterStatus):
+                            if msg.key == "client_encoding":
+                                self._client_encoding = msg.value
+                        elif isinstance(msg, Protocol.BackendKeyData):
+                            self._backend_key_data = msg
+                        elif isinstance(msg, Protocol.ErrorResponse):
+                            raise msg.createException()
+                        else:
+                            raise InternalError("unexpected msg %r" % msg)
                 else:
                     raise InterfaceError("authentication method %s failed" % msg.__class__.__name__)
             else:
                 raise InternalError("StartupMessage was responded to with non-AuthenticationRequest msg %r" % msg)
 
-        def _waitForReady(self):
-            while 1:
-                msg = self._read_message()
-                if isinstance(msg, Protocol.ReadyForQuery):
-                    self.state = "ready"
-                    break
-                elif isinstance(msg, Protocol.ErrorResponse):
-                    raise msg.createException()
-
         def extended_query(self, portal, statement, qs, params):
             self.verifyState("ready")
             self._send(Protocol.Parse(statement, qs, [type(x) for x in params]))
-            self._send(Protocol.Bind(portal, statement, (0,), params, (0,)))
+            self._send(Protocol.Bind(portal, statement, (0,), params, (0,), self._client_encoding))
             self._send(Protocol.DescribePortal(portal))
             self._send(Protocol.Flush())
             while 1:
@@ -655,7 +662,19 @@ class Protocol(object):
                 elif isinstance(msg, Protocol.CommandComplete):
                     self._send(Protocol.ClosePortal(portal))
                     self._send(Protocol.Sync())
-                    self._waitForReady()
+                    while 1:
+                        msg = self._read_message()
+                        if isinstance(msg, Protocol.ReadyForQuery):
+                            # ready to move on with life...
+                            self._state = "ready"
+                            break
+                        elif isinstance(msg, Protocol.CloseComplete):
+                            # ok, great!
+                            pass
+                        elif isinstance(msg, Protocol.ErrorResponse):
+                            raise msg.createException()
+                        else:
+                            raise InternalError("unexpected response msg %r" % msg)
                     end_of_data = True
                     break
                 elif isinstance(msg, Protocol.ErrorResponse):
@@ -669,7 +688,7 @@ class Protocol(object):
             self._send(Protocol.Query(qs))
             msg = self._read_message()
             if isinstance(msg, Protocol.RowDescription):
-                self.state = "in_query"
+                self._state = "in_query"
                 return msg
             elif isinstance(msg, Protocol.ErrorResponse):
                 raise msg.createException()
@@ -712,7 +731,7 @@ class Types(object):
         return type_oid
     pg_type_id = staticmethod(pg_type_id)
 
-    def pg_value(v, fc):
+    def pg_value(v, fc, client_encoding):
         typ = type(v)
         data = Types.py_types.get(typ)
         if data == None:
@@ -724,7 +743,7 @@ class Types(object):
             func = func_bin
         if func == None:
             raise NotSupportedError("type %r, format code %r not converted" % (typ, fc))
-        return func(v)
+        return func(v, client_encoding)
     pg_value = staticmethod(pg_value)
 
     def py_value(data, description):
@@ -754,12 +773,6 @@ class Types(object):
     def int4in(data, description):
         return int(data)
 
-    def int4out(v):
-        return str(v)
-
-    def int4send(v):
-        return struct.pack("!i", v)
-
     def timestamp_recv(data, description):
         val = struct.unpack("!d", data)[0]
         return datetime.datetime(2000, 1, 1) + datetime.timedelta(seconds = val)
@@ -779,14 +792,14 @@ class Types(object):
         else:
             return decimal(data)
 
-    def numeric_out(v):
+    def numeric_out(v, ce):
         return str(v)
 
     def varcharin(data, description):
         return unicode(data, "utf-8")
 
-    def varcharout(v):
-        return v.encode("utf-8")
+    def varcharout(v, ce):
+        return v.encode(ce)
 
     py_types = {
         int: (1700, numeric_out, None),
