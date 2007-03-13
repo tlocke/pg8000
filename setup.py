f6858f39c922d4cc0d5149963d7dee3b2e07738b
mfenniak <devnull@localhost>
2007-03-13 14:00:23 -0600

    [svn] update setup.py for version 1.02.

--- setup.py
+++ setup.py
@@ -2,28 +2,27 @@
 
 from distutils.core import setup
 
-long_description = """
-pg8000 is a Pure-Python interface to the PostgreSQL database engine.  It is one
+long_description = \
+"""pg8000 is a Pure-Python interface to the PostgreSQL database engine.  It is one
 of many PostgreSQL interfaces for the Python programming language.  pg8000 is
 somewhat distinctive in that it is written entirely in Python and does not rely
 on any external libraries (such as a compiled python module, or PostgreSQL's
-libpq library).
+libpq library).  pg8000 supports the standard Python DB-API version 2.0.
 
 pg8000's name comes from the belief that it is probably about the 8000th
-PostgreSQL interface for Python.
-"""
+PostgreSQL interface for Python."""
 
 setup(
         name="pg8000",
-        version="1.01",
+        version="1.02",
         description="PostgreSQL interface library",
         long_description=long_description,
         author="Mathieu Fenniak",
         author_email="biziqe@mathieu.fenniak.net",
         url="http://pybrary.net/pg8000/",
-        download_url="http://pybrary.net/pg8000/pg8000-1.01.tar.gz",
+        download_url="http://pybrary.net/pg8000/pg8000-1.02.tar.gz",
         classifiers = [
-            "Development Status :: 3 - Alpha",
+            "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: BSD License",
             "Programming Language :: Python",
@@ -31,6 +30,6 @@ setup(
             "Topic :: Database :: Front-Ends",
             "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-        py_modules = ["pg8000"],
+        py_modules = ["pg8000", "pg8000_dbapi"],
     )
 
