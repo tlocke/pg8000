1c40ade3c20bdf5e12d84a98ef938297cb41a457
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2008-05-12 12:25:30 -0600

    add example.py

---
+++ example.py
@@ -0,0 +1,18 @@
+from pg8000 import DBAPI
+
+db = DBAPI.connect(
+        host="192.168.111.128",
+        user="unittest",
+        password="unittest",
+        database="pg8000")
+
+cursor = db.cursor()
+cursor.execute("CREATE TEMPORARY TABLE book (id SERIAL, title TEXT, author TEXT)")
+
+cursor.execute(
+        "INSERT INTO book (title, author) VALUES (%s, %s) RETURNING id",
+        ("Ender's Game", "Orson Scott Card"))
+book_id, = cursor.fetchone()
+
+
+
