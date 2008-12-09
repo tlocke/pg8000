5cffb28595f5f41fba959476fda454087a348c81
Mathieu Fenniak <mathieu.fenniak@replicon.com>
2008-12-09 10:32:23 -0700

    improve test closing

--- tests/error_recovery.py
+++ tests/error_recovery.py
@@ -1,5 +1,6 @@
 import unittest
 from pg8000 import DBAPI, DatabaseError
+from contextlib import closing
 from .connection_settings import db_connect
 
 db = DBAPI.connect(**db_connect)
@@ -18,27 +19,28 @@ class Tests(unittest.TestCase):
         from pg8000 import types
         original_py_value = types.py_value
         types.py_value = self.raiseException
-        try:
-            c = db.cursor()
+        with closing(db.cursor()) as c:
             try:
-                c.execute("VALUES ('hw1'::text), ('hw2'::text)")
-                c.fetchall()
-                # shouldn't get here, exception should be thrown
-                self.fail()
-            except TestException:
-                # should be TestException type, this is OK!
-                pass
-        finally:
-            types.py_value = original_py_value
+                try:
+                    c.execute("VALUES ('hw1'::text), ('hw2'::text)")
+                    c.fetchall()
+                    # shouldn't get here, exception should be thrown
+                    self.fail()
+                except TestException:
+                    # should be TestException type, this is OK!
+                    pass
+            finally:
+                types.py_value = original_py_value
 
-        # ensure that the connection is still usable for a new query
-        c.execute("VALUES ('hw3'::text)")
-        self.assert_(c.fetchone()[0] == "hw3")
+            # ensure that the connection is still usable for a new query
+            c.execute("VALUES ('hw3'::text)")
+            self.assert_(c.fetchone()[0] == "hw3")
 
     def testNoDataErrorRecovery(self):
         for i in range(1, 4):
             try:
-                db.cursor().execute("DROP TABLE t1")
+                with closing(db.cursor()) as cursor:
+                    cursor.execute("DROP TABLE t1")
             except DatabaseError as e:
                 # the only acceptable error is:
                 self.assert_(e.args[1] == b'42P01', # table does not exist
@@ -52,3 +54,4 @@ class Tests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
