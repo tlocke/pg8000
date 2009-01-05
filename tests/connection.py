b2bd7cbfeba2378a37b0e096c98814806e8f1cf4
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2009-01-05 12:43:16 -0700

    change notify tests to use dbapi interface

--- tests/connection.py
+++ tests/connection.py
@@ -1,38 +1,37 @@
+from __future__ import with_statement
+
 import unittest
-import pg8000
+from pg8000 import dbapi
 from contextlib import closing
 from .connection_settings import db_connect
 
 # Tests related to connecting to a database.
 class Tests(unittest.TestCase):
     def testSocketMissing(self):
-        self.assertRaises(pg8000.InterfaceError, pg8000.Connection,
+        self.assertRaises(dbapi.InterfaceError, dbapi.connect,
                 unix_sock="/file-does-not-exist", user="doesn't-matter")
 
     def testDatabaseMissing(self):
         data = db_connect.copy()
         data["database"] = "missing-db"
-        self.assertRaises(pg8000.ProgrammingError, pg8000.Connection, **data)
+        self.assertRaises(dbapi.ProgrammingError, dbapi.connect, **data)
 
     def testNotify(self):
-        self._notify = None
         try:
-            db = pg8000.Connection(**db_connect)
-            db.NotificationReceived += self._notifyReceived
-            try:
-                db.execute("LISTEN test")
-                db.execute("NOTIFY test")
-                db.execute("VALUES (1, 2), (3, 4), (5, 6)")
-                self.assert_(self._notify != None)
-                self.assertEquals("test", self._notify.condition)
-            finally:
-                db.NotificationReceived -= self._notifyReceived
-                del self._notify
+            db = dbapi.connect(**db_connect)
+            self.assert_(db.notifies == [])
+
+            with closing(db.cursor()) as cursor:
+                cursor.execute("LISTEN test")
+                cursor.execute("NOTIFY test")
+                db.commit()
+
+                cursor.execute("VALUES (1, 2), (3, 4), (5, 6)")
+                self.assert_(len(db.notifies) == 1)
+                self.assert_(db.notifies[0][1] == "test")
+
         finally:
             db.close()
-
-    def _notifyReceived(self, msg):
-        self._notify = msg
 
 
 if __name__ == "__main__":
