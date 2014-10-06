54f4c18eaed5d1e336d1243564b2d8ca1c3b721f
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2014-10-06 17:12:57 -0600

    Fix Python 2.6 build

--- circleci/install-python-2.6.9.sh
+++ circleci/install-python-2.6.9.sh
@@ -7,7 +7,8 @@ if [[ ! -e py-2.6.9/bin/python2.6 ]]; then
     wget https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
     tar -zxf Python-2.6.9.tgz
     cd ./Python-2.6.9
-    ./configure --prefix=$BUILDROOT/py-2.6.9
+    # LDFLAGS here makes the build detect libssl correctly, which makes _hashlib build.
+    LDFLAGS="-L/usr/lib/x86_64-linux-gnu" ./configure --prefix=$BUILDROOT/py-2.6.9
     make install
     cd $BUILDROOT
 fi
