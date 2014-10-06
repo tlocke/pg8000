d7b934128e688cb1ba35a13766bd855f0a67c80d
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2014-10-06 09:35:32 -0600

    Workaround for Python 2.5 build error

--- circleci/install-python-2.5.6.sh
+++ circleci/install-python-2.5.6.sh
@@ -7,7 +7,8 @@ if [[ ! -e py-2.5.6/bin/python ]]; then
     wget https://www.python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2
     tar -jxf Python-2.5.6.tar.bz2
     cd ./Python-2.5.6
-    ./configure --prefix=$BUILDROOT/py-2.5.6
+    # LDFLAGS fixes explicit definition avoids http://bugs.python.org/issue1706863
+    LDFLAGS="-L/usr/lib/x86_64-linux-gnu" ./configure --prefix=$BUILDROOT/py-2.5.6
     make install
     cd $BUILDROOT
 fi
