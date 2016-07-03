a76cacfdc1f8b86de65e0595de9cf566aa85aa65
Mathieu Fenniak <mathieu@encouragemarketing.com>
2016-07-03 14:09:34 -0600

    Add a second PG 9.5 instance configured for doctests

--- circleci/run-postgres-and-tox.sh
+++ circleci/run-postgres-and-tox.sh
@@ -3,6 +3,7 @@
 ./pgsql-9.3.13/bin/postgres -D `pwd`/pgsql-9.3.13/data -p 5493 &> pgsql-9.3.log &
 ./pgsql-9.4.8/bin/postgres -D `pwd`/pgsql-9.4.8/data -p 5494 &> pgsql-9.4.log &
 ./pgsql-9.5.3/bin/postgres -D `pwd`/pgsql-9.5.3/data -p 5495 &> pgsql-9.5.log &
+./pgsql-9.5.3/bin/postgres -D `pwd`/pgsql-9.5.3/data2 -p 5432 &> pgsql-9.5-data2.log &
 
 cd ~postgres/pg8000-src/
 tox
