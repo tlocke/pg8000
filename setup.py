7d3ede29b362d2ae777752c41b9ade8fe05ac32e
Tony Locke <tlocke@tlocke.org.uk>
2019-02-06 11:01:17 +0000

    Python 2 & 3 release
    We weren't correctly uploading releases to PyPI, which led to confusion
    when dropping Python 2 compatibility. Thanks to
    https://github.com/piroux[Pierre Roux] for his
    https://github.com/tlocke/pg8000/issues/7[detailed explanation] of what
    went wrong and how to correct it.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     author_email="biziqe@mathieu.fenniak.net",
     url="https://github.com/tlocke/pg8000",
     license="BSD",
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
         "six>=1.10.0",
     ],
