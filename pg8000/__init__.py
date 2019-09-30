1456802bb5b7d1e1af84cd9e901d9d8339bb8004
David King <davidking@davids-mbp.home>
2019-09-30 14:19:37 +0100

    Allow setting of connection source address

--- pg8000/__init__.py
+++ pg8000/__init__.py
@@ -41,13 +41,14 @@ __author__ = "Mathieu Fenniak"
 
 
 def connect(
-        user, host='localhost', unix_sock=None, port=5432, database=None,
-        password=None, ssl=None, timeout=None, application_name=None,
-        max_prepared_statements=1000, tcp_keepalive=True):
+        user, host='localhost', source_address=None, unix_sock=None, port=5432,
+        database=None, password=None, ssl=None, timeout=None,
+        application_name=None, max_prepared_statements=1000,
+        tcp_keepalive=True):
 
     return Connection(
-        user, host, unix_sock, port, database, password, ssl, timeout,
-        application_name, max_prepared_statements, tcp_keepalive)
+        user, host, source_address, unix_sock, port, database, password, ssl,
+        timeout, application_name, max_prepared_statements, tcp_keepalive)
 
 
 apilevel = "2.0"
