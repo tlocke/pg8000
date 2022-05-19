adc2f71803ceb4dff2ac67f4b78c62c12d67cc2c
Tony Locke <tlocke@tlocke.org.uk>
2022-05-19 19:34:21 +0100

    Change README format to rst
    This is to be compatible with PEP517.

--- test/test_readme.py
+++ test/test_readme.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 def test_readme():
     failure_count, _ = doctest.testfile(
-        str(Path("..") / "README.adoc"), verbose=False, optionflags=doctest.ELLIPSIS
+        str(Path("..") / "README.rst"), verbose=False, optionflags=doctest.ELLIPSIS
     )
     assert failure_count == 0
